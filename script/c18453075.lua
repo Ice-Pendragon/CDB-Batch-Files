12e01153f98477a0ee278aa7b9ed201053fcc0b9
Crowel <faithtaeho@naver.com>
2024-08-07 16:55:16 +0900

    Revert "Add files via upload"
    This reverts commit 03d1bf1b884bffcbd6c30c0f4dbb1235ac0ae8f6.

--- script/c18453075.lua
+++
@@ -1,8 +0,0 @@
---사일런트 머조리티: 1
-local m=18453075
-local cm=_G["c"..m]
-function cm.initial_effect(c)
-	aux.AddSquareProcedure(c)
-end
-cm.square_mana={ATTRIBUTE_WIND}
-cm.custom_type=CUSTOMTYPE_SQUARE
